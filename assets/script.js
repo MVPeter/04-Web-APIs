c1da314c4e3141c5bb4904d0fe2d2ccbbf3a6223
MattV <mvpetersen1@gmail.com>
2021-01-09 18:40:05 -0700

    updated Readme, updated questions, removed logs updated event for 0 time left

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
-var questions1 = [["Question 1", "How Many Colors", "Correct Answer 1"], ["Correct Answer 1", "Wrong Answer 11", "Wrong Answer 12", "Wrong Answer 13"]];
-var questions2 = [["Question 2", "What is 1 + 1", "Correct Answer 22"], ["Wrong Answer 2", "Wrong Answer 21", "Correct Answer 22", "Wrong Answer 23"]];
-var questions3 = [["Question 3", "What is 2 + 1", "Correct Answer 31"], ["Wrong Answer 3", "Correct Answer 31", "Wrong Answer 32", "Wrong Answer 33"]];
+var questions1 = [["Question 1", "What is used to separate elements in an array?", "A comma , "], ["A comma , ", "A semicolon ; ", "A question mark ? ", "Wrong Answer 13"]];
+var questions2 = [["Question 2", "What should be at the end of each line?", "A semicolon ; "], ["A period . ", "Two front slashes // ", "A semicolon ; ", "Wrong Answer 23"]];
+var questions3 = [["Question 3", "Which answer sets a variable?", "var i = 0 "], ["x", "var i = 0 ", "function() ", "Wrong Answer 33"]];
 var questions4 = [["Question 4", "What is 3 + 4", "Correct Answer 4"], ["Correct Answer 4", "Wrong Answer 41", "Wrong Answer 42", "Wrong Answer 43"]];
 
 
@@ -13,8 +13,6 @@ var questionNumber = document.getElementById("questionNumber")
 var startButton = document.getElementById("startB");
 var qSubmit = document.querySelector("#submitQ");
 var msgDiv = document.querySelector("#msg");
-var playerScore = "";
-var playerHighScore = "";
 var highScoreBoard = document.getElementById("highScore1");
 var radio1 = document.getElementById("answer1");
 var radio2 = document.getElementById("answer2");
@@ -24,11 +22,9 @@ var radioText2 = document.getElementById("answer2Text");
 var radioText3 = document.getElementById("answer3Text");
 var updateArticle = document.getElementById("mainArticle");
 var updateImg = document.getElementById("startImg");
-// var startingImg = "./assets/quiz.jpg";
+var countDownId = 0;
 var qCount = 0;
-var aCount = 0;
 var timeStart = 30;
-var secs = 0;
 var tScore = localStorage.getItem("topScore");
 
 if (tScore == null) {
@@ -38,52 +34,9 @@ if (tScore == null) {
 
 highScoreBoard.innerHTML = localStorage.getItem("topName") + ": " + localStorage.getItem("topScore");
 
-
-// var timer = setInterval(function () {
-//     countDown--;
-//     if (countDown === 0) {
-//         clearInterval(timer);
-//         localStorage.setItem("score", countDown);
-
-//     } else {
-//         localStorage.setItem("score", countDown);
-//         clearInterval(timer);
-//     }
-// }, 1000);
-// function starttimer() {
-//     declimentTime = setInterval("declimentTime()", 1000);
-// }
-
-// function declimentTime() {
-//     if(timeStart > 0 ) {
-//         timeStart = timeStart - 1; 
-//         timerEl.innterHTML = timeStart;
-//     } else {
-//         clearInterval()
-//     }
-
-// }
-
-// function countDown() {
-//     // var element = document.getElementById(elem);
-//     timerEl.innerHTML = timeStart;
-//     console.log(qCount + "  Countdown")
-//     if (timeStart < 1 || qCount === 5) {
-//         clearTimeout(timer);
-//         timerEl.innerHTML = ""
-//     } else {
-
-//     }
-//         timeStart--;
-//         var timer = setTimeout('countDown(' + timeStart + ')', 1000);
-
-
-// };
-
-
-// var clock = 10;
 var countDownId = 0;
 
+//Timer
 function startTimer() {
     countDownId = setInterval("countDown()", 1000);
 }
@@ -92,8 +45,18 @@ function countDown() {
     if (timeStart > 0) {
         timeStart = timeStart - 1;
         timerEl.innerHTML = timeStart;
-    } else if (qCount >= 3) {
+    } else
+    checkTimer()
+}
+
+function checkTimer() {
+    if (timeStart === 0) {
+        for (var i = 0; i < radioSelection.length; i++)
+            radioSelection[i].checked = false;
+        updateScore()
         clearInterval(countDownId);
+        resetImage()
+
     }
 }
 
@@ -111,11 +74,8 @@ function resetImage() {
 
 // Function to remote the Image and replace it with the Main article
 function mainArticle(questionsNum) {
-    timeStart = 30
     var qNum = "questions" + qCount.toString()
-    console.log(questionsNum + "           ArticleUpdate");
-    console.log(qCount + " qCount after article update");
-    var divQuestion = document.createElement("p");
+    // var divQuestion = document.createElement("p");
     updateImg.setAttribute("class", "hide");
     questionQ.removeAttribute("class", "hide");
     updateQuestionText(qNum);
@@ -125,36 +85,27 @@ function mainArticle(questionsNum) {
 function updateQuestionText(questionsNum) {
     var qNum = "questions" + qCount.toString();
     var arrayName = window[qNum];
-    console.log(questionsNum + "[0][1]");
-    console.log(qCount + "       qCount after question update");
     var questionA = document.getElementById("questionA");
     questionA.innerHTML = arrayName[0][1];
-    console.log(qNum + "     RadioUpdate");
-    console.log(qCount);
     radioText1.innerHTML = arrayName[1][0];
     radio1.value = arrayName[0][2]
     radioText2.innerHTML = arrayName[1][1];
     radio1.value = arrayName[0][2]
     radioText3.innerHTML = arrayName[1][2];
     radio1.value = arrayName[0][2]
-    // qCount++;
-    console.log(qCount + " after ++");
-
 }
 
 // Start questions
 startButton.addEventListener("click", function (event) {
     event.preventDefault();
-    localStorage.setItem("score", 0);
+    timeStart = 30
     startTimer();
-    // console.log("hit");
+    localStorage.setItem("score", 0);
+    msgDiv.innerHTML = "";
     qCount++;
     var qNum = "questions" + qCount.toString()
-    console.log(qNum + "    StartButton");
-
     // grab player name and save it to local storage
     var playerName = document.querySelector("#player").value;
-    // console.log(playerName + " clicked");
     localStorage.setItem("Name", playerName);
 
     // setup main article
@@ -163,32 +114,12 @@ startButton.addEventListener("click", function (event) {
 
 });
 
-// function checkSelection() {
-//     var radioSelection = document.getElementsByName("answer");
-//     var qNum = "questions" + qCount.toString();
-//     var arrayName = window[qNum];
-//     var correctAnswer = arrayName[0][2]
-//     // var aSelection = radioText1;
-//     for (var i = 0; i < radioSelection.length; i++);{
-//     if (radio[i].checked && radiotext[i] === correctAnswer) {
-//         console.log( answer[i]);
-//         updateQuestionText(qNum);
-//     } else {
-//         timeStart = timeStart - 10;
-//         localStorage.setItem("score", timeStart);
-//         updateQuestionText(qNum);
-//         // timeStart--;
-
-//     }}
-// }
-
+// Check for correct answer
 function correctAnswerCheck(qNum, text) {
     var qNum = "questions" + qCount.toString();
     var arrayName = window[qNum];
-    console.log(qNum);
     var correctAnswer = arrayName[0][2];
-    console.log(correctAnswer + "from array");
-    console.log(text + "  from radio selection");
+
     if (text === correctAnswer) {
         qCount++;
         updateQuestionText(qNum);
@@ -200,6 +131,7 @@ function correctAnswerCheck(qNum, text) {
     }
 }
 
+// Check which radio is active
 function checkSelection(qNum, text) {
     if (radio1.checked === true) {
         text = radioText1.textContent;
@@ -219,11 +151,11 @@ function checkSelection(qNum, text) {
 
 }
 
+//Submit button for each question.
 qSubmit.addEventListener("click", function (event) {
     event.preventDefault();
     var radioSelection = document.getElementsByName("answer");
-    if (qCount < 3) {
-        // var radioSelection = document.getElementsByName("answer");
+    if (qCount < 3 && timeStart > 0) {
         //check for correct answer
         var qNum = "questions" + qCount.toString();
         checkSelection(qNum)
@@ -231,21 +163,17 @@ qSubmit.addEventListener("click", function (event) {
 
         for (var i = 0; i < radioSelection.length; i++)
             radioSelection[i].checked = false;
-        //run next question
-        // updateQuestionText(qNum);
-
     } else {
-        console.log("Updating top Score.")
         for (var i = 0; i < radioSelection.length; i++)
             radioSelection[i].checked = false;
         updateScore()
         clearInterval(countDownId);
         timerEl.innerHTML = ""
         resetImage()
-
     }
 });
 
+//Update score, set localstorage items
 function updateScore() {
     localStorage.setItem("score", timeStart);
     var tNScore = Number(localStorage.getItem("topScore"));
@@ -258,98 +186,4 @@ function updateScore() {
         localStorage.setItem("topScore", lScore);
         highScoreBoard.innerHTML = localStorage.getItem("topName") + ": " + localStorage.getItem("topScore");
     }
-
-    //Update DOM 'highScore1'
-    // }
 }
-
-
-// Create a timer 40 to 0 seconds
-// function startTimer() {
-
-// }
-
-// function countDownTimer() {
-//     countDown--;
-//     if(CountDown ===0) {
-//         clear
-//     }
-// });
-
-
-
-// var timeInterval = setInterval(function() {
-//     timerE1.textContent = timeleft + "seconds";
-//     timeleft--;
-//     if (timeleft ===0) {
-//     }
-
-
-
-// check corrent answer for question
-
-// function answerQuestion1(A1) {
-//     var correctAnswer = questions1[0, 1];
-//     switch (A1) {
-//         case correctAnswer:
-//             // update text with 'Correct'
-//             displayMessage("correct", "Correct:  Next question");
-//             // continue to next question
-
-//             break;
-//         default:
-//             // update text with "Incorrent"
-//             displayMessage("incorrect", "Incorrect:  -10 seconds");
-//         // Remove 10 seconds from timer
-//         // continue to next quesiton   
-//     }
-
-//     if (a1 == questions1[0, 1]) {
-//         alert("correct")
-//         startAgain()
-//     } else alert("dumbby")
-// }
-
-// function answerQuestion2(A2) {
-//     var correctAnswer = questions2[0,1];
-//     switch(A2) {
-//         case correctAnswer:
-//             // update text with 'Correct'
-//             displayMessage("correct", "Correct:  Next question");
-//             // continue to next question
-
-//         break;
-//      default:
-//             // update text with "Incorrent"
-//             displayMessage("incorrect", "Incorrect:  -10 seconds");
-//             // Remove 10 seconds from timer
-//             // continue to next quesiton   
-//         }
-// }
-
-
-
-//update radio answers
-// function updateRadioText(questionsNum) {
-
-//     radioText1.textContent = questionsNum[1][1];
-//     radioText2.textContent = questionsNum[1][2];
-//     radioText3.textContent = questionsNum[1][3];
-// }
-
-// // count down timer
-// var timerLeft = setInterval(function () {
-
-
-// }, 1000);
-
-// Subtrack from local store timer 
-// function removeTenSec() {
-//     count--;
-//     counter.textContent = count;
-
-//     localStorage.setItem("score", count);
-// };
-
-
-// Check High score.
